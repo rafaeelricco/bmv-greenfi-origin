413878d9050c42a1939763155e83da158cd518e1
rafaeelricco <rafaelricco10@gmail.com>
2024-11-02 14:56:16 -0300

    chore: updated code format using prettier and resolved building errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,12 +45,15 @@ export default function RootLayout({
                href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap"
                rel="stylesheet"
             />
+            {/* eslint-disable-next-line @next/next/no-page-custom-font */}
             <link rel="preconnect" href="https://fonts.googleapis.com" />
+            {/* eslint-disable-next-line @next/next/no-page-custom-font */}
             <link
                rel="preconnect"
                href="https://fonts.gstatic.com"
                crossOrigin="anonymous"
             />
+            {/* eslint-disable-next-line @next/next/no-page-custom-font */}
             <link
                href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
                rel="stylesheet"
